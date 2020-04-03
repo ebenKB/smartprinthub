c1fc3750dcf0e7fc6f11cee1e8cb96e24adefadc
ebenKB <eakbo23@gmail.com>
2020-04-03 22:07:04 +0000

    initial commit

---
+++ src/components/signin/signin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './signin.scss';
+
+const SignIn = () => {
+  return (
+    <div>
+      This is where users will login
+    </div>
+  )
+}
+
+export default SignIn
