d0f27c43c2ea36d4df6f9f402e134e7693a56693
ebenKB <eakbo23@gmail.com>
2020-08-11 09:30:57 +0000

    improve user profile icon add new caret svg icon

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import './UserProfile.scss';
 import { Link } from 'react-router-dom';
 import Divider from '../divider/divider';
+import { ReactComponent as DownArrow } from '../../svg/down-arrow.svg';
 
 const UserProfile = () => (
 	<div className="user-profile">
-		User eben
+		<div className="target flex-inline">
+			<span>Eben_KB</span>
+			<DownArrow className="x-small icon" />
+		</div>
 		<div className="user-profile__content">
 			<div className="dropdown-item">
 				<div className="flex-inline flex center">
@@ -17,31 +21,41 @@ const UserProfile = () => (
 				</div>
 			</div>
 			<Divider type="faint" />
-			<div className="dropdown-item">
-				<Link to="/" className="link">
-					My Profile
-				</Link>
+			<div className="dropdown-item__wrapper">
+				<div className="dropdown-item">
+					<Link to="/" className="link">
+						My Profile
+					</Link>
+				</div>
 			</div>
-			<div className="dropdown-item">
-				<Link to="/" className="link">
-					Settings
-				</Link>
+			<div className="dropdown-item__wrapper">
+				<div className="dropdown-item">
+					<Link to="/" className="link">
+						Settings
+					</Link>
+				</div>
 			</div>
-			<div className="dropdown-item">
-				<Link to="/" className="link">
-					My Companies
-				</Link>
+			<div className="dropdown-item__wrapper">
+				<div className="dropdown-item">
+					<Link to="/" className="link">
+						My Companies
+					</Link>
+				</div>
 			</div>
-			<div className="dropdown-item">
-				<Link to="/" className="link">
-					Notifications
-				</Link>
+			<div className="dropdown-item__wrapper">
+				<div className="dropdown-item">
+					<Link to="/" className="link">
+						Notifications
+					</Link>
+				</div>
 			</div>
 			<Divider type="faint" />
-			<div className="dropdown-item">
-				<Link to="/" className="link">
-					Help
-				</Link>
+			<div className="dropdown-item__wrapper">
+				<div className="dropdown-item">
+					<Link to="/" className="link">
+						Help
+					</Link>
+				</div>
 			</div>
 			<div className="dropdown-item">
 				<Link to="/" className="link">
