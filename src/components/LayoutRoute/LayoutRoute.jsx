39bf472da436057a4a6d20cb990789c4c98a5495
ebenKB <eakbo23@gmail.com>
2020-09-01 15:56:26 +0000

    enforce protected route

--- src/components/LayoutRoute/LayoutRoute.jsx
+++ src/components/LayoutRoute/LayoutRoute.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-self-compare */
+/* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect, Suspense } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import Layout from '../layout/layout';
 
@@ -13,11 +15,15 @@ const LayoutRoute = ({ component: Component, ...rest }) => {
 	<Route
 		{...rest}
 		render={(matchProps) => (
+		  (1 === 1) ? (
 			<Suspense fallback={<Layout>loading</Layout>}>
 				<Layout>
 					<Component {...matchProps} />
 				</Layout>
 			</Suspense>
+		  ) : (
+			<Redirect to="/signin" />
+		  )
 		)}
 	/>
   );
