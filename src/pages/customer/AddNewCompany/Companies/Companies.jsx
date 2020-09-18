61b4099c10e08e403ec6a4da89eb4a85a228c8db
ebenKB <eakbo23@gmail.com>
2020-09-18 11:50:24 +0000

    refactor routes

---
+++ src/pages/customer/AddNewCompany/Companies/Companies.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Companies = () => (
+	<div>
+		Show the companies that the customer has over here
+	</div>
+);
+
+export default Companies;
--- src/routes/customer-protected-routes.jsx
+++ src/routes/customer-protected-routes.jsx
@@ -1,14 +1,13 @@
 import React, { lazy } from 'react';
 import Transactions from '../pages/customer/Transactions/Transactions';
 import AddNewCompany from '../pages/customer/AddNewCompany/AddNewCompany';
-// import Test from '../pages/customer/create-job/create-job';
+import Companies from '../pages/customer/AddNewCompany/Companies/Companies';
 
 const CreateJob = lazy(() => import('../pages/customer/CreateJob/CreateJob'));
 const ViewJobs = lazy(() => import('../pages/customer/jobs/ViewJobs/ViewJobs'));
 const ShowJob = lazy(() => import('../pages/customer/jobs/show/show-job'));
 const UserSettings = lazy(() => import('../pages/customer/settings/settings'));
 const Settings = lazy(() => import('../pages/customer/CustomerSettingsTabpane/CustomerSettingsTabpane'));
-// const Checkout = lazy(() => import('../pages/customer/checkout/checkout'));
 const Checkout = lazy(() => import('../pages/customer/PaystackCheckout/PaystackCheckout'));
 const ConfirmCheckout = lazy(() => import('../pages/customer/ConfirmCheckout/ConfirmCheckout'));
 
@@ -20,54 +19,50 @@ const routes = [
   },
   {
     path: '/job/create',
-    exact: true,
     main: () => <CreateJob />,
     title: 'Create Job',
   },
   {
     path: '/jobs',
-    exact: true,
     main: () => <ViewJobs />,
     title: 'Jobs',
   },
   {
     path: '/jobs/view/:id',
-    exact: true,
     main: () => <ShowJob />,
     title: 'Job Details',
   },
   {
     path: '/user/settings/:page',
-    exact: true,
     main: () => <Settings />,
     title: 'Settings',
   },
   {
     path: '/user/settings/old',
-    exact: true,
     main: () => <UserSettings />,
   },
   {
     path: '/job/checkout',
-    exact: true,
     main: () => <Checkout />,
     title: 'Checkout',
   },
   {
     path: '/job/checkout/confirm',
-    exact: true,
     main: () => <ConfirmCheckout />,
     title: 'Confirm Checkout',
   },
   {
     path: '/user/transactions',
-    exact: true,
     main: () => <Transactions />,
     title: 'Transactions',
   },
   {
+    path: '/companies',
+    main: () => <Companies />,
+    title: 'Companies',
+  },
+  {
     path: '/companies/new',
-    exact: true,
     main: () => <AddNewCompany />,
     title: 'Add Company',
   },
